393f5bda3292e2c2258415cf9962618a69d50478
Fredrik Jansson <fjansson@abo.fi>
2015-05-01 13:04:05 +0200

    fixed history drawing when scrolling

--- src/display.c
+++ src/display.c
@@ -351,8 +351,8 @@ void draw_bot_history(SDL_Surface *surface, int w, int h, kilobot *bot)
       Uint32 ui_color = conv_RGBA(85 * bot->r_led, 85 * bot->g_led, 85 * bot->b_led, (int) i_alpha);
       i_alpha = i_alpha * 0.998;
       filledCircleColor(surface,
-			display_w/2 + display_scale * bot->x_history[i],
-			display_h/2 + display_scale * bot->y_history[i],
+			display_w/2 + display_scale * (bot->x_history[i] - c_x),
+			display_h/2 + display_scale * (bot->y_history[i] - c_y),
 			display_scale * 2, ui_color);
     }
   }
