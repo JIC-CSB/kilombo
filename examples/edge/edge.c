0be8d0c1538a2a894b4b4b26470b9ac17ea47385
Fredrik Jansson <fjansson@abo.fi>
2015-10-04 12:40:56 +0100

    Adding Ivicas edge following example, slightly polished

---
+++ examples/edge/edge.c
@@ -0,0 +1,304 @@
+/* Kilobot Edge following demo
+ * 
+ * Ivica Slavkov, Fredrik Jansson  2015
+ */
+
+#include <math.h>
+#include <stdint.h>   // for uint8_t and friends
+#include "kilolib.h"
+
+#include "edge.h"
+
+enum {STOP,LEFT,RIGHT,STRAIGHT};
+
+typedef struct
+{
+  Neighbor_t neighbors[MAXN];
+
+  int N_Neighbors;
+  uint8_t bot_type;
+  uint8_t bot_state;
+  uint8_t move_type;
+  uint8_t move_switch_flag;
+  uint8_t listen_counter;
+
+  message_t transmit_msg;
+  char message_lock;
+  uint32_t next_ticks;
+  received_message_t RXBuffer[RB_SIZE];
+  uint8_t RXHead, RXTail;
+
+} USERDATA;
+
+#include <userdata.h>
+
+#ifdef SIMULATOR
+#include <stdio.h> // for printf
+#else
+#include <avr/io.h>   // for microcontroller register defs
+#define DEBUG         // for printf to serial port
+#include "debug.h"
+#endif
+
+
+uint8_t colorNum[] = {
+  RGB(0,0,0),  //0 - off
+  RGB(1,0,0),  //1 - red
+  RGB(0,1,0),  //2 - green
+  RGB(0,0,1),  //3 - blue
+  RGB(1,1,0),  //4 - yellow
+  RGB(0,1,1),  //5 - cyan
+  RGB(1,0,1),  //6 - purple
+  RGB(2,1,0),  //7  - orange
+  RGB(1,1,1),  //8  - white
+  RGB(3,3,3)   //9  - bright white
+};
+
+
+
+
+// message rx callback function. Pushes message to ring buffer.
+void rxbuffer_push(message_t *msg, distance_measurement_t *dist) {
+    received_message_t *rmsg = &RB_back();
+    rmsg->msg = *msg;
+    rmsg->dist = *dist;
+    RB_pushback();
+}
+
+
+message_t *message_tx()
+{
+  if (mydata->message_lock)
+    return 0;
+  return &mydata->transmit_msg;
+}
+
+void set_bot_state(int state)
+{
+  mydata->bot_state = state;
+}
+
+int get_bot_state(void)
+{
+  return mydata->bot_state;
+}
+
+void set_move_type(int type)
+{
+  mydata->move_type = type;
+}
+
+int get_move_type(void)
+{
+  return mydata->move_type;
+}
+
+/* Process a received message at the front of the ring buffer.
+ * Go through the list of neighbors. If the message is from a bot
+ * already in the list, update the information, otherwise
+ * add a new entry in the list
+ */
+
+void process_message()
+{
+  uint8_t i;
+  uint16_t ID;
+
+  uint8_t *data = RB_front().msg.data;
+  ID = data[0] | (data[1] << 8);
+  uint8_t d = estimate_distance(&RB_front().dist);
+
+
+  // search the neighbor list by ID
+  for (i = 0; i < mydata->N_Neighbors; i++)
+    if (mydata->neighbors[i].ID == ID)
+      {// found it
+    	break;
+      }
+
+  if (i == mydata->N_Neighbors){  // this neighbor is not in list
+    if (mydata->N_Neighbors < MAXN-1) // if we have too many neighbors,
+      mydata->N_Neighbors++;          // we overwrite the last entry
+                                      // sloppy but better than overflow
+  }
+
+  // i now points to where this message should be stored
+  mydata->neighbors[i].ID = ID;
+  mydata->neighbors[i].timestamp = kilo_ticks;
+  mydata->neighbors[i].dist = d;
+  mydata->neighbors[i].N_Neighbors = data[2];
+  mydata->neighbors[i].n_bot_state = data[3];
+}
+
+
+
+/* Go through the list of neighbors, remove entries older than a threshold,
+ * currently 2 seconds.
+ */
+void purgeNeighbors(void)
+{
+  int8_t i;
+
+  for (i = mydata->N_Neighbors-1; i >= 0; i--)
+    if (kilo_ticks - mydata->neighbors[i].timestamp  > 64) //32 ticks = 1 s
+      { //this one is too old.
+	mydata->neighbors[i] = mydata->neighbors[mydata->N_Neighbors-1]; //replace it by the last entry
+	mydata->N_Neighbors--;
+      }
+}
+
+
+//void printNeighbors(void)
+//{
+//  uint8_t i;
+//  printf ("UID:%d N:%d state:%d  move: %d, t:%lu\n", kilo_uid, mydata->N_Neighbors, get_bot_state(), get_move_type(), (unsigned long)kilo_ticks);
+//  printf ("\n");
+//  for (i = 0; i < mydata->N_Neighbors; i++)
+//    printf ("  ID: %2d, dist: %2d, delta_dist: %3d, state: %d, time:%lu\n", mydata->neighbors[i].ID,
+//	    mydata->neighbors[i].dist, mydata->neighbors[i].delta_dist, mydata->neighbors[i].n_bot_state,(unsigned long int)mydata->neighbors[i].timestamp);
+//
+//  printf ("\n");
+//}
+
+void setup_message(void)
+{
+  mydata->message_lock = 1;  //don't transmit while we are forming the message
+  mydata->transmit_msg.type = NORMAL;
+  mydata->transmit_msg.data[0] = kilo_uid & 0xff; //0 low  ID
+  mydata->transmit_msg.data[1] = kilo_uid >> 8;   //1 high ID
+  mydata->transmit_msg.data[2] = mydata->N_Neighbors;     //2 number of neighbors
+  mydata->transmit_msg.data[3] = get_bot_state();
+
+  mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);
+  mydata->message_lock = 0;
+}
+
+void setup() {
+
+  rand_seed(kilo_uid + 1); //seed the random number generator
+  
+  mydata->message_lock = 0;
+  mydata->next_ticks = 0;
+
+  mydata->N_Neighbors = 0;
+  mydata->move_switch_flag = 0;
+  set_move_type(STOP);
+  set_bot_state(LISTEN);
+
+  setup_message();
+}
+
+void receive_inputs()
+{
+  while (!RB_empty())
+    {
+      process_message();
+      RB_popfront();
+    }
+  purgeNeighbors();
+}
+
+
+
+uint8_t get_dist_by_ID(uint16_t bot)
+{
+  uint8_t i;
+  uint8_t dist = 255;
+  
+  for(i = 0; i < mydata->N_Neighbors; i++)
+    {
+      if(mydata->neighbors[i].ID == bot)
+	{
+	  dist = mydata->neighbors[i].dist;
+	  break;
+	}
+    }
+  return dist;
+}
+
+uint8_t find_nearest_N_dist()
+{
+  uint8_t i;
+  uint8_t dist = 90;
+
+  for(i = 0; i < mydata->N_Neighbors; i++)
+    {
+      if(mydata->neighbors[i].dist < dist)
+	{
+	  dist = mydata->neighbors[i].dist;
+	}
+    }
+  return dist;
+}
+
+void follow_edge()
+{
+  uint8_t desired_dist = 55;
+  if(find_nearest_N_dist() > desired_dist)
+    {
+      if(get_move_type() == LEFT) spinup_motors();
+      set_motors(0, kilo_turn_right);
+      set_move_type(RIGHT);
+    }
+
+  if(find_nearest_N_dist() < desired_dist)
+    {
+      if(get_move_type() == RIGHT) spinup_motors();
+      set_motors(kilo_turn_left, 0);
+      set_move_type(LEFT);
+    }
+}
+
+void loop()
+{
+  //receive messages
+  receive_inputs();
+  if(kilo_uid == 0)
+    follow_edge();
+
+  setup_message();
+}
+
+extern char* (*callback_botinfo) (void);
+char *botinfo(void);
+
+int main(void)
+{
+  kilo_init();
+
+#ifdef DEBUG
+  debug_init();
+#endif
+
+#ifdef SIMULATOR
+  register_callback(CALLBACK_BOTINFO, (void(*)(void))botinfo);
+  register_callback(CALLBACK_RESET, setup);
+
+#endif
+
+
+  RB_init();                       // initialize ring buffer
+  kilo_message_rx = rxbuffer_push;
+  kilo_message_tx = message_tx;    // register our transmission function
+
+  kilo_start(setup, loop);
+
+  return 0;
+}
+
+#ifndef KILOBOT
+static char botinfo_buffer[10000];
+// provide a text string for the status bar, about this bot
+char *botinfo(void)
+{
+  int n;
+  char *p = botinfo_buffer;
+  n = sprintf (p, "ID: %d ", kilo_uid);
+  p += n;
+
+  n = sprintf (p, "Ns: %d, dist: %d\n ", mydata->N_Neighbors, find_nearest_N_dist());
+  p += n;
+
+  return botinfo_buffer;
+}
+#endif
---
+++ examples/edge/edge.h
@@ -0,0 +1,114 @@
+#ifndef GRN_H
+#define GRN_H
+
+#include "kilolib.h"
+
+#ifdef KILOBOT  // physical kilobot
+
+#else           //  simulator
+  #include <stdio.h>     // for printf
+ #endif
+
+
+#include <math.h>
+#include <stdint.h>   // for uint8_t and friends
+
+#ifndef M_PI
+#define M_PI 3.141592653589793238462643383279502884197169399375105820974944
+#endif
+
+
+#define GENES 6
+
+#define DIFFGENES 6  // Maximal number of diffusing genes, limited by the message length.
+
+#define MAXN 20
+#define RB_SIZE 16  // Ring buffer size. Choose a power of two for faster code
+                    // memory usage: 16*RB_SIZE
+                    // 8 works too, but complains in the simulator
+                    // when the bots are very dense
+
+extern uint8_t NGenes;
+
+enum BOTTYPE {LAST, FOLLOWER, LEADER};
+enum BOTSTATES {WAIT, LISTEN, MOVE};
+
+typedef struct {
+  uint16_t ID;
+  uint8_t dist;
+//  int delta_dist;
+
+  uint8_t n_bot_state;
+  uint8_t N_Neighbors;
+  uint32_t timestamp;
+
+} Neighbor_t;
+
+typedef struct {
+    message_t msg;
+    distance_measurement_t dist;
+} received_message_t;
+
+
+
+// Ring buffer operations. Taken from kilolib's ringbuffer.h
+// but adapted for use with mydata->
+
+// Ring buffer operations indexed with head, tail
+// These waste one entry in the buffer, but are interrupt safe:
+//   * head is changed only in popfront
+//   * tail is changed only in pushback
+//   * RB_popfront() is to be called AFTER the data in RB_front() has been used
+//   * head and tail indices are uint8_t, which can be updated atomically
+//     - still, the updates need to be atomic, especially in RB_popfront()
+
+#define RB_init() {	\
+    mydata->RXHead = 0; \
+    mydata->RXTail = 0;\
+}
+
+#define RB_empty() (mydata->RXHead == mydata->RXTail)
+
+#define RB_full()  ((mydata->RXHead+1)%RB_SIZE == mydata->RXTail)
+
+#define RB_front() mydata->RXBuffer[mydata->RXHead]
+
+#define RB_back() mydata->RXBuffer[mydata->RXTail]
+
+#define RB_popfront() mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;
+
+#define RB_pushback() {\
+    mydata->RXTail = (mydata->RXTail+1)%RB_SIZE;\
+    if (RB_empty())\
+      { mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;	\
+	printf("Full.\n"); }				\
+  }
+
+
+/*
+// Ring buffer operations indexed with head, count
+// These save one entry in the buffer, but are less interrupt safe due to the shared count
+
+#define RB_init() {	\
+    mydata->RXHead = 0; \
+    mydata->RXCount = 0;\
+}
+#define RB_empty() (mydata->RXCount == 0)
+#define RB_full()  (mydata->RXCount == RB_SIZE)
+#define RB_front() (mydata->RXBuffer[mydata->RXHead])
+#define RB_back()  (mydata->RXBuffer[(mydata->RXHead+mydata->RXCount)%RB_SIZE])
+#define RB_popfront() {\
+    mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;\
+    mydata->RXCount--;\
+}
+
+#define RB_pushback() {\
+    if (RB_full())\
+        mydata->RXHead = (mydata->RXHead+1)%RB_SIZE;\
+    else\
+      mydata->RXCount++;\
+}
+*/
+
+
+#endif
